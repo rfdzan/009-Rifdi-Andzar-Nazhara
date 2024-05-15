fd4fa606ef63221d5f5d4b346d67187c3830688f
rfdzan <hekodev.py@gmail.com>
2024-05-15 12:59:46 +0700

    (WIP) fixing test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,15 +5,24 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class RouteTest extends TestCase
 {
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $appUrl = env('APP_URL');
+        $routes = ['/', '/registration', '/fulan'];
+        foreach ($routes as $route) {
+            $response = $this->get($route);
+            if ((int)$response()->status() !== 200) {
+                echo $appUrl . $route . '(FAILED) route did not return 200';
+                $this->assertTrue(false);
+            } else {
+                echo $appUrl . $route . '(SUCCESS) route returned 200';
+                $this->assertTrue(true);
+            }
+        }
     }
 }
