860490ab41ffa57ee6d01bba9f78998501c22bf3
rfdzan <hekodev.py@gmail.com>
2024-05-17 14:25:28 +0700

    user page shows author's work, mocking db data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\ImageController;
+use Illuminate\Contracts\View\View;
+
 class UserController extends Controller
 {
-    function getUser(string $user)
+    function getPlaceHolderAuthors(): array
     {
-        return view('user', ['name' => $user]);
+        return array('bob' => '104827', 'jole' => '416160', 'luna' => '45201', 'rudy' => '57416', 'elena' => '96938');
+    }
+    function getUserPage(string $user): View
+    {
+        $image_db = new ImageController();
+        $artworks = [];
+        foreach ($image_db->getPath() as $id => $path) {
+            if (strcmp($id, $this->getPlaceHolderAuthors()[$user]) === 0) {
+                array_push($artworks, (object)['id' => $id, 'path' => $path]);
+            }
+        }
+        return view('user', ['name' => $user, 'artworks' => $artworks]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,6 +4,9 @@
 <section>
     <div class="section-user-artworks">
         <h2>Works</h2>
+        @foreach ($artworks as $art)
+        <a href="artwork?id={{$art->id}}"><img src="{{$art->path}}"></a>
+        @endforeach
     </div>
     <div class="section-user-sidebar">
         <h2>{{$name}}</h2>
