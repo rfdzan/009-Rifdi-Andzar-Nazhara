30ef1887ed1059fb7037f1796c0573dac7904ead
rfdzan <hekodev.py@gmail.com>
2024-05-22 18:05:28 +0700

    fe: artwork page styling

--- resources/views/artwork.blade.php
+++ resources/views/artwork.blade.php
@@ -1,16 +1,19 @@
 @extends('layout.default')
 @section('content')
 <!-- artwork section -->
-<section class="artwork">
-    <h2>{{$artwork_name}}</h2>
-    <img src="{{$path}}" width="500px">
-    <section class="artwork-description">
-        <h3>Author:</h3>
-        <div>
-            <a href="/{{$author}}">
-                <p>{{$author}}</p>
-            </a>
-        </div>
-    </section>
+<link rel="stylesheet" href="css/artwork/artwork.css">
+<section id="artwork">
+    <div class="div-artwork">
+        <h2>{{$artwork_name}}</h2>
+        <img src="{{$path}}" width="500px">
+        <section class="artwork-description">
+            <h3>Author:</h3>
+            <div>
+                <a href="/{{$author}}">
+                    <p>{{$author}}</p>
+                </a>
+            </div>
+        </section>
+    </div>
 </section>
 @endsection
