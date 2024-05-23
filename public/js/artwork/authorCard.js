c0e01312052364aeee813e32c85842eb9d55d2db
rfdzan <hekodev.py@gmail.com>
2024-05-23 17:35:13 +0700

    refactor for clarity

--- public/js/artwork/authorCard.js
+++ public/js/artwork/authorCard.js
@@ -1,19 +1,25 @@
+function moveAuthorCard(authorCard, moveDown) {
+  if (moveDown) {
+    authorCard.style.marginLeft = "0px";
+    authorCard.style.marginRight = "0px";
+    authorCard.style.width = "100%";
+  }
+  authorCard.style.marginLeft = "5px";
+  authorCard.style.marginRight = "5px";
+  authorCard.style.width = "18%";
+}
 function moveDown() {
   let authorCard = document.getElementById("author-card");
   let artworkCard = document.getElementById("artwork-card");
   let artworkSection = document.getElementById("artwork");
   if (window.innerWidth < 900) {
     artworkSection.style.flexDirection = "column";
-    authorCard.style.width = "100%";
     artworkCard.style.width = "100%"
-    authorCard.style.marginLeft = "0px";
-    authorCard.style.marginRight = "0px";
+    moveAuthorCard(authorCard, true);
   } else {
     artworkSection.style.flexDirection = "row";
-    authorCard.style.width = "18%";
     artworkCard.style.width = "80%";
-    authorCard.style.marginLeft = "5px";
-    authorCard.style.marginRight = "5px";
+    moveAuthorCard(authorCard, false);
   }
 }
 window.addEventListener("resize", moveDown)
