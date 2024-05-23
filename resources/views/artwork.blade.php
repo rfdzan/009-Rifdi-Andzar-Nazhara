e9d1b1ccda0e088f8af58de53d43aa66deb6be94
rfdzan <hekodev.py@gmail.com>
2024-05-23 12:41:01 +0700

    fe: styling artwork page

--- resources/views/artwork.blade.php
+++ resources/views/artwork.blade.php
@@ -3,17 +3,18 @@
 <!-- artwork section -->
 <link rel="stylesheet" href="css/artwork/artwork.css">
 <section id="artwork">
-    <div class="div-artwork">
-        <h2>{{$artwork_name}}</h2>
-        <img src="{{$path}}" width="500px">
-        <section class="artwork-description">
+    <div class="artwork-card">
+        <div class="artwork-container">
+            <img src="{{$path}}" width="500px">
+            <h2>{{$artwork_name}}</h2>
+        </div>
+    </div>
+    <div class="author-card">
+        <div class="author-container">
             <h3>Author:</h3>
-            <div>
-                <a href="/{{$author}}">
-                    <p>{{$author}}</p>
-                </a>
-            </div>
-        </section>
+            <a class="author-link" href="/{{$author}}">
+                <p>{{$author}}</p>
+        </div>
     </div>
 </section>
 @endsection
