803936e74ce5d8ac8bd9d180b5da2b2684996f8f
rfdzan <hekodev.py@gmail.com>
2024-05-24 10:38:54 +0700

    fe: creating upload pop up form

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -3,6 +3,22 @@
 <!-- User section -->
 <link rel="stylesheet" href="{{asset('css/user/user.css')}}">
 <section class="user-page-section">
+    <div class="upload-button-container">
+        <button class="upload-button">Upload</button>
+    </div>
+    <!-- upload -->
+    <div class="popup-form">
+        <form class="user-upload-form" action="{{ route('artwork_upload', $name)}}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div>
+                <label for="file">Upload</label>
+                <input id="upload-artwork" type="file" name="file[]" accept="image/*" multiple>
+                <input type="submit" value="Submit">
+            </div>
+            <button id="clear-file">Clear</button>
+        </form>
+        <script src="{{asset('js/upload.js')}}"></script>
+    </div>
     <div class="user-page">
         <div class="user-artworks">
             @foreach ($artworks as $art)
@@ -13,21 +29,7 @@
             </a>
             @endforeach
         </div>
-        <!-- upload -->
-        <div class="popup-form">
-            <div class="user-upload">
-                <form class="user-upload-form" action="{{ route('artwork_upload', $name)}}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <div>
-                        <label for="file">Upload</label>
-                        <input id="upload-artwork" type="file" name="file[]" accept="image/*" multiple>
-                        <input type="submit" value="Submit">
-                    </div>
-                    <button id="clear-file">Clear</button>
-                </form>
-                <script src="{{asset('js/upload.js')}}"></script>
-            </div>
-        </div>
+        <!-- user desc -->
         <div class="user-description">
             <h2>{{$name}}</h2>
             <p>Description: </p>
