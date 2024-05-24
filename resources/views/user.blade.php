e860d1577de7efb9a25c9f82b34f0f30e91cada6
rfdzan <hekodev.py@gmail.com>
2024-05-24 10:15:06 +0700

    fe: user page

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,30 +2,36 @@
 @section('content')
 <!-- User section -->
 <link rel="stylesheet" href="{{asset('css/user/user.css')}}">
-<div class="div-user-artworks">
-    <div class="user-works-title">
-        <h2>Works</h2>
-    </div>
-    <div class="user-upload">
-        <form class="user-upload-form" action="{{ route('artwork_upload', $name)}}" method="post" enctype="multipart/form-data">
-            @csrf
-            <div>
-                <label for="file">Upload</label>
-                <input id="upload-artwork" type="file" name="file[]" accept="image/*" multiple>
-                <input type="submit" value="Submit">
+<section class="user-page-section">
+    <div class="user-page">
+        <div class="user-artworks">
+            @foreach ($artworks as $art)
+            <a class="user-artwork-link" href="artwork?id={{$art->id}}">
+                <div class="user-artwork-container">
+                    <img src="{{$art->path}}">
+                </div>
+            </a>
+            @endforeach
+        </div>
+        <!-- upload -->
+        <div class="popup-form">
+            <div class="user-upload">
+                <form class="user-upload-form" action="{{ route('artwork_upload', $name)}}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div>
+                        <label for="file">Upload</label>
+                        <input id="upload-artwork" type="file" name="file[]" accept="image/*" multiple>
+                        <input type="submit" value="Submit">
+                    </div>
+                    <button id="clear-file">Clear</button>
+                </form>
+                <script src="{{asset('js/upload.js')}}"></script>
             </div>
-        </form>
-        <button id="clear-file">Clear</button>
-        <script src="{{asset('js/upload.js')}}"></script>
-    </div>
-    @foreach ($artworks as $art)
-    <a href="artwork?id={{$art->id}}"><img src="{{$art->path}}" width="300px"></a>
-    @endforeach
-</div>
-<div class="div-user-sidebar">
-    <h2>{{$name}}</h2>
-    <p>Description: </p>
-</div>
+        </div>
+        <div class="user-description">
+            <h2>{{$name}}</h2>
+            <p>Description: </p>
+        </div>
 </section>
 
 @endsection
